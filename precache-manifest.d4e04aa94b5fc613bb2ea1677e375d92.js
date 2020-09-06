963967ade35b98b3cf80c5152c1bb766206a70ee
young hun <thddudrb79@naver.com>
2020-09-06 17:16:15 +0900

    Updates

---
+++ precache-manifest.d4e04aa94b5fc613bb2ea1677e375d92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1007d71a5a50eefba36bd27e84bd7758",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c3606fe2d512f4da922a",
+    "url": "/robofriends/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3606fe2d512f4da922a",
+    "url": "/robofriends/static/js/main.cb0e7b5f.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
